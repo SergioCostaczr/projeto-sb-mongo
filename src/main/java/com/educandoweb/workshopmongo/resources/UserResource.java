4c1d692419615bdc77f26ffb6ce24d15a580c129
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-01-05 04:05:35 -0300

    feat: adiciona busca de usuário por id com tratamento de exceções

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.educandoweb.workshopmongo.dto.UserDto;
 import com.educandoweb.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,9 +20,15 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> findAll(){
+    public ResponseEntity<List<UserDto>> findAll() {
         List<User> list = service.findAll();
-        List<UserDto> listDto = list.stream().map(x-> new UserDto(x)).collect(Collectors.toList());
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDto(user));
     }
 }
