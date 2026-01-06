fc96c3f69f85122dcce5577597bcd652fe3c35c4
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-01-06 03:15:24 -0300

    feat: consulta simples com @Query

--- src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.educandoweb.workshopmongo.repository;
 import com.educandoweb.workshopmongo.domain.Post;
 import com.educandoweb.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")//{ <field>: { $regex: /pattern/, $options: '<options>' } }
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text); //Consulta com query methods
 }
